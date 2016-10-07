bbdd000e1b1bc187b9ecbfbfa096cb5b74e6c080
Dany Cortez <danielitos0901@ciencias.unam.mx>
2016-10-06 20:31:20 -0500

    Add files via upload
    Se agrego el codigo de pyhton para que las interfaces se muestresn y que las celdas de la tabla se ajusten

---
+++ cliente.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt4 import QtGui,QtCore, uic
+
+class interfaz_cliente(QtGui.QMainWindow):
+    def __init__(self):
+        super(interfaz_cliente, self).__init__()
+        uic.loadUi('cliente.ui', self)
+        self.tableWidget.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        self.tableWidget.verticalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        self.show()
+
+snake = QtGui.QApplication(sys.argv)
+window = interfaz_cliente()
+sys.exit(snake.exec_())
